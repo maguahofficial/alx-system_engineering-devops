8d2b4e7bd09b6032eb07ea027c76810d144b1c6a
maguah <maguahofficial@gmail.com>
2024-03-23 05:32:00 +0300

    0x0A. Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
 #!/usr/bin/pup
 # This installs a especific version of flask (2.1.0)
-package {'flask':
-  version  => '2.1.0',
-  provider => 'pip3',
+exec { 'puppet-lint':
+  thecommand => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
 }
