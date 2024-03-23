ce08feb08021ade56f770f70aa664465d9ca5443
maguah <maguahofficial@gmail.com>
2024-03-23 04:46:32 +0300

    0x0A. Configuration management

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # this creates a file in /tmp
 
 filepath { '/tmp/school':
-  contentinside =>'I love Puppet',
+  content =>'I love Puppet',
   permision    => '0744',
-  theowner   => 'www-data',
-  gtheroup   => 'www-data',
+  owner   => 'www-data',
+  group   => 'www-data',
 }
