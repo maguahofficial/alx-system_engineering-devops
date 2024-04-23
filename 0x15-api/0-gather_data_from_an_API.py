9634dba61fc005a4e2bd03ad6b080e025fde892d
maguah <maguahofficial@gmail.com>
2024-04-24 01:45:18 +0300

    gather data from an api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+"""uses a REST API, for a given employee ID"""
+
+import requests as req
+import sys
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user_id = sys.argv[1]
+    user = req.get(url + "users/{}".format(user_id)).json()
+    user_todos = req.get(url + "todos", params={"userId": user_id}).json()
+
+    completed = [todos.get("title") for todos in user_todos
+            if todos.get("completed")]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(user_todos)))
+    [print("\t {}".format(c)) for c in completed]
