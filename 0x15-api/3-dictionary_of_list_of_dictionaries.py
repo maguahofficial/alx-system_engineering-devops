59b5407cdad3f1f872829343276a0bf9ad7ebe14
maguah <maguahofficial@gmail.com>
2024-04-24 02:05:46 +0300

    export to json

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Records all tasks from all employees"""
+import json
+import requests as req
+
+if __name__ == "__main__":
+
+    url = "https://jsonplaceholder.typicode.com/"
+    users = req.get(url + "users").json()
+
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump({
+            u.get("id"): [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": u.get("username")
+            } for t in req.get(url + "todos",
+                               params={"userId": u.get("id")}).json()]
+            for u in users}, jsonfile)
