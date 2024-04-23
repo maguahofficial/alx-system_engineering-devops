850ae5926c07794fa0a37bb77d5eb7c5f1e0c849
maguah <maguahofficial@gmail.com>
2024-04-24 01:47:21 +0300

    gather data from an api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     user_todos = req.get(url + "todos", params={"userId": user_id}).json()
 
     completed = [todos.get("title") for todos in user_todos
-            if todos.get("completed")]
+        if todos.get("completed")]
     print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(completed), len(user_todos)))
     [print("\t {}".format(c)) for c in completed]
