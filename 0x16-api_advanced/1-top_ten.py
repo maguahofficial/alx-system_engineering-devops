582b48edda1a0ac4e4a515673707d0501eb879b5
maguah <maguahofficial@gmail.com>
2024-05-08 03:12:42 +0300

    0x16-api_advanced

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,11 +10,11 @@ def top_ten(subreddit):
     headers = {'User-Agent': 'xica369'}
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     parameters = {'limit': 10}
-    responses = req.get(url, headers=headers, allow_redirects=False,
+    response = req.get(url, headers=headers, allow_redirects=False,
                        params=parameters)
 
-    if responses.status_code == 200:
-        titles_ = responses.json().get('data').get('children')
+    if response.status_code == 200:
+        titles_ = response.json().get('data').get('children')
         for title_ in titles_:
             print(title_.get('data').get('title'))
     else:
